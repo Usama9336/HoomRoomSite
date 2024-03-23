82dcff6ea28d697f0d006ad91cc6f772fb5d1d26
unknown <arshadusama9336@gmail.com>
2024-03-23 16:51:11 +0530

    h

--- src/Home.jsx
+++ src/Home.jsx
@@ -45,11 +45,11 @@ const Home = () => {
     </div>
 </div>
 <div className='flex flex-col items-center space-y-[2rem] sm:py-[7rem] py-[4rem] '>
-    <div className='flex flex-col items-center text-[white] space-y-[0.5rem]'>
+    <div className='flex flex-col items-center text-[white] space-y-[0.5rem] mx-[0.7rem]'>
 <p className='font-sans font-medium text-[18px]'>
 THE BEST WAY TO
 </p>
-<p className='font-sans font-bold text-[30px] sm:text-[44px] text-center'>
+<p className='font-sans font-bold text-[28px] sm:text-[44px] text-center'>
 Compare Prices Of What You Want
 </p>
 </div>
@@ -69,7 +69,7 @@ Compare Prices Of What You Want
         </TextField>
     <TextField id="outlined-search" label="Search " type="search" className='rounded-xl w-[100%] sm:w-[58%] bg-[white] ' />
     <Link to="/nextpage">
-    <button className='bg-gradient-to-r from-[#83C44C] to-[#149C78] py-[0.9rem] text-[white] mx-[4.4rem] sm:mx-[0px] sm:px-[2.5rem] shadow-xl mt-[1rem] sm:mt-[0px] '>
+    <button className='bg-gradient-to-r from-[#83C44C] to-[#149C78] py-[0.9rem] text-[white] mx-[4.5rem] sm:mx-[0px] sm:px-[2.5rem] shadow-xl mt-[1rem] sm:mt-[0px] '>
         <div className='flex gap-[8px] items-center'>
             <div className='w-[20%]'>
         <img src={vector} alt="" />
