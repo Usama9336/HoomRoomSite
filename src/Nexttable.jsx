5c0f32ad4bb19adce4be2c418ea04f123725dccc
Usama Arshad <109479121+Usama9336@users.noreply.github.com>
2024-03-28 15:46:05 +0530

    Update Nexttable.jsx

--- src/Nexttable.jsx
+++ src/Nexttable.jsx
@@ -7,29 +7,28 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData( calories, fat, carbs, protein, srp, mrp,gst,range) {
-  return {  calories, fat, carbs, protein,srp,mrp,gst,range };
+function createData( fat, carbs, protein, srp, mrp,gst) {
+  return { fat, carbs, protein,srp,mrp,gst };
 }
 
 const rows = [
-  createData( 159, 6.0, 24, 4.0,'₹ 99,999','₹ 99,999','₹ 99,999','₹ 99,999')
+  createData( '', '', '','₹ 99,999','₹ 99,999','₹ 99,999'),
 ];
 
 export default function Nexttable() {
   return (
-    <TableContainer component={Paper} style={{ maxHeight: 280, overflowY: 'auto' }} >
+    <TableContainer component={Paper} style={{ maxHeight: 270, overflowY: 'auto' }} >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead >
+          <p className='text-[#404040] text-[
+20.11px] font-bold ml-[0.8rem] mt-[0.6rem]'>HEADING</p>
           <TableRow>
-          <TableCell>
-            <p className='text-[13px] text-[#565656] font-bold'> Code</p>
-            </TableCell>
           <TableCell>
             <p className='text-[13px] text-[#565656] font-bold' >SDP</p>
             
             </TableCell>
-          <TableCell>
-          <p className='text-[13px] text-[#565656] font-bold' >NRP</p>
+          <TableCell >
+          <p className='ml-[-2rem] sm:ml-[-3.5rem] text-[13px] text-[#565656] font-bold' >NRP</p>
             
             </TableCell>
           <TableCell>
@@ -37,19 +36,16 @@ export default function Nexttable() {
             
             </TableCell>
           <TableCell>
-          <p className='text-[13px] text-[#565656] font-bold' > SDP Incl GST</p>
+          <p className='text-[13px] text-[#565656] font-bold' > SDP To MRP</p>
             
             </TableCell>
             <TableCell >
-            <p className='text-[13px] text-[#565656] font-bold' > Diff SDP & NRP Inc GST</p>
+            <p className='text-[13px] text-[#565656] font-bold' > SDP To NRP</p>
              
               </TableCell>
             <TableCell >
-            <p className='text-[13px] text-[#565656] font-bold' >  Diff SDP & MRP Inc GST</p>
+            <p className='text-[13px] text-[#565656] font-bold' > MRP To NRP</p>
             
-              </TableCell>
-            <TableCell >
-            <p className='text-[13px] text-[#565656] font-bold' > Range Code</p>
               </TableCell>
            
           </TableRow>
@@ -61,9 +57,8 @@ export default function Nexttable() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               
-              <TableCell >{row.calories}</TableCell>
               <TableCell>{row.fat}</TableCell>
-              <TableCell>{row.carbs}</TableCell>
+              <TableCell><p className='ml-[-2rem] sm:ml-[-3.5rem]'>{row.carbs}</p></TableCell>
               <TableCell >{row.protein}</TableCell>
               <TableCell ><p className='bg-gradient-to-r from-[#83C44C] to-[#149C78] inline-block text-transparent bg-clip-text '>{row.srp}</p></TableCell>
               <TableCell >
@@ -74,11 +69,7 @@ export default function Nexttable() {
               <p className='bg-gradient-to-r from-[#83C44C] to-[#149C78] inline-block text-transparent bg-clip-text '>{row.gst}</p>
                 
                 </TableCell>
-              <TableCell >
-              <p className='bg-gradient-to-r from-[#83C44C] to-[#149C78] inline-block text-transparent bg-clip-text '>{row.range}</p>
-                
-                
-                </TableCell>
+             
             </TableRow>
           ))}
         </TableBody>
